758a6bd759649b352d129d30340690a8c1183f37
moeyg <moooeyg@gmail.com>
2024-09-19 12:52:51 +0900

    feat: navigation search 기능 추가

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useRef, useState } from 'react';
 import * as S from './Navigation.style';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Navigation() {
+  const [inputVisible, setInputVisible] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const navigate = useNavigate();
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const query = new URLSearchParams(search);
+    const text = query.get('q');
+    if (text) setSearchValue(text);
+  }, [search]);
+
+  const handleSearchClick = () => {
+    setInputVisible((prev) => !prev);
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`, { replace: true });
+    if (!inputVisible && inputRef.current) inputRef.current.focus();
+  };
+
   return (
     <S.Nav>
       <S.Logo
@@ -10,10 +34,18 @@ export default function Navigation() {
       />
       <S.Menu>
         <S.Search>
-          <S.SearchInput />
+          {inputVisible && (
+            <S.SearchInput
+              ref={inputRef}
+              value={searchValue}
+              type='text'
+              onChange={handleSearch}
+            />
+          )}
           <S.SearchIcon
             src='/public/images/search-icon.svg'
             alt='search icon'
+            onClick={handleSearchClick}
           />
         </S.Search>
         <S.Profile
