0386cbb96122b4ce1b09b99abdadd0dae3c40170
moeyg <moooeyg@gmail.com>
2024-09-19 12:23:43 +0900

    style: navigation 스타일링

---
+++ src/components/Navigation/Navigation.style.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  padding: 0 1rem;
+  border-bottom: 1px solid #d9d9d9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 3;
+
+  @media (max-width: 450px) {
+    padding: -0.3rem 1rem;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 4.2rem;
+  cursor: pointer;
+  display: inline-block;
+
+  @media (max-width: 450px) {
+    width: 3rem;
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+
+  @media (max-width: 450px) {
+    gap: 0.7rem;
+  }
+`;
+
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  @media (max-width: 450px) {
+    gap: 0.5rem;
+  }
+`;
+
+export const SearchIcon = styled.img`
+  width: 2.5rem;
+  cursor: pointer;
+
+  @media (max-width: 450px) {
+    width: 1.8rem;
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 15rem;
+  font-size: 1.2rem;
+  background-color: #d9d9d9;
+  border: none;
+  border-radius: 25px;
+  padding: 7px;
+  color: black;
+  outline: none;
+
+  @media (max-width: 450px) {
+    width: 9rem;
+    padding: 3px;
+  }
+`;
+
+export const Profile = styled.img`
+  width: 2.5rem;
+  cursor: pointer;
+
+  @media (max-width: 450px) {
+    width: 1.8rem;
+  }
+`;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,26 @@
+import * as S from './Navigation.style';
+
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <S.Nav>
+      <S.Logo
+        src='/public/images/logo.svg'
+        alt='35MM Logo'
+        onClick={() => (window.location.href = '/')}
+      />
+      <S.Menu>
+        <S.Search>
+          <S.SearchInput />
+          <S.SearchIcon
+            src='/public/images/search-icon.svg'
+            alt='search icon'
+          />
+        </S.Search>
+        <S.Profile
+          src='/public/images/profile-icon.svg'
+          alt='user profile icon'
+        />
+      </S.Menu>
+    </S.Nav>
+  );
 }
