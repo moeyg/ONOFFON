7cbe8db0df1012740ae59d776e78bd82461a8025
moeyg <moooeyg@gmail.com>
2024-09-20 14:59:38 +0900

    feat: 인기 콘텐츠 목록을 보여주는 캐러셀 기능 추가

--- src/components/MovieCarousel/MovieCarousel.style.ts
+++ src/components/MovieCarousel/MovieCarousel.style.ts
@@ -1,1 +1,48 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+export const MovieGenre = styled.h2`
+  font-size: 2rem;
+  margin-top: 3rem;
+  margin-bottom: 0.5rem;
+
+  @media screen and (max-width: 600px) {
+    font-size: 1.3rem;
+  }
+`;
+
+export const Movie = styled.article`
+  width: 95%;
+  height: 100%;
+  padding-top: 150%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  border: 3px solid rgba(108, 98, 98, 0.1);
+
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px --16px,
+      rgb(0 0 0 / 73%) 0px 30px 22px -10px;
+    transform: scale(0.99);
+    border-color: rgba(249, 249, 249, 0.4);
+  }
+`;
+
+export const MoviePoster = styled.img`
+  inset: 0px;
+  display: block;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  opacity: 1;
+  position: absolute;
+  transition: opacity 500ms ease-in-out;
+  z-index: 1;
+  filter: grayscale(50%);
+`;
--- src/components/MovieCarousel/MovieCarousel.tsx
+++ src/components/MovieCarousel/MovieCarousel.tsx
@@ -1,8 +1,12 @@
 import * as S from './MovieCarousel.style';
+import './MovieCarousel.css';
+
+import { useCallback, useEffect, useState } from 'react';
 import axios from '../../api/axios.ts';
+import Modal from '../Modal/Modal.tsx';
+
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Modal from '../Modal/Modal.tsx';
 
 // swiper style
 import 'swiper/css';
@@ -10,13 +14,85 @@ import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import 'swiper/css/pagination';
 
-interface Props {
+interface MovieData {
   genre: string;
   fetchUrl: string;
 }
 
-const MovieCarousel = ({ genre, fetchUrl }) => {
-  return <div>asdfasdf</div>;
+interface Movie {
+  id: number;
+  poster_path: string;
+  name: string;
+  title?: string;
+}
+
+const MovieCarousel: React.FC<MovieData> = ({ genre, fetchUrl }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState({});
+
+  const fetchMoviesData = useCallback(async () => {
+    try {
+      const response = await axios.get(fetchUrl);
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }, [fetchUrl]);
+
+  const openModal = (movie: Movie) => {
+    setModalOpen(true);
+    setMovie(movie);
+  };
+
+  useEffect(() => {
+    fetchMoviesData();
+  }, [fetchMoviesData]);
+
+  return (
+    <S.Container>
+      <S.MovieGenre>{genre}</S.MovieGenre>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={15}
+        breakpoints={{
+          1378: {
+            slidesPerView: 9,
+            slidesPerGroup: 9,
+          },
+          998: {
+            slidesPerView: 8,
+            slidesPerGroup: 8,
+          },
+          625: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          0: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+        }}
+      >
+        {movies.map((movie: Movie) => (
+          <SwiperSlide key={movie.id}>
+            <S.Movie onClick={() => openModal(movie)}>
+              <S.MoviePoster
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt={movie.name}
+              />
+            </S.Movie>
+          </SwiperSlide>
+        ))}
+        {modalOpen && (
+          <Modal setModalOpen={setModalOpen} {...movie} genre={genre} />
+        )}
+      </Swiper>
+    </S.Container>
+  );
 };
 
 export default MovieCarousel;
