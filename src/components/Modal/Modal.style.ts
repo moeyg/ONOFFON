f4606ac273db739a67361e0bdf9a799d6d6943e5
moeyg <moooeyg@gmail.com>
2024-09-24 23:50:21 +0900

    feat: 영화 모달 기능 추가

---
+++ src/components/Modal/Modal.style.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+export const Presentation = styled.article`
+  position: absolute;
+  z-index: 200;
+`;
+
+export const Container = styled.section`
+  position: fixed;
+  width: 100%;
+  inset: 0px;
+  background-color: rgba(0, 0, 0, 0.85);
+  display: flex;
+  justify-content: center;
+
+  @media (max-height: 768px) {
+    align-items: unset;
+    padding: 0%;
+    overflow-y: scroll;
+  }
+`;
+
+export const Modal = styled.section`
+  position: relative;
+  max-width: 800px;
+  box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12);
+  background-color: var(--background-color);
+  animation: fadeIn 400ms;
+  overflow-y: scroll !important;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: scale(0.5);
+    }
+    to {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+`;
+
+export const Close = styled.button`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+  z-index: 1000;
+  color: var(--primary-color);
+  font-size: 1rem;
+  background-color: var(--background-color);
+  width: 1.9rem;
+  height: 1.9rem;
+  border: 1px solid var(--background-color);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MovieBackdrop = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const MovieContents = styled.section`
+  padding: 1.3rem;
+  color: var(--primary-color);
+`;
+
+export const MovieGenre = styled.p`
+  font-weight: 600;
+  line-height: 1.5;
+  opacity: 0.9;
+`;
+
+export const MovieTitle = styled.h2`
+  padding: 0;
+  font-size: 2rem;
+  font-family: 'pretendard-bold';
+  margin: 1rem 0;
+
+  @media screen and (max-width: 850px) {
+    font-size: 1.7rem;
+  }
+
+  @media screen and (max-height: 768px) {
+    overflow-y: scroll !important;
+  }
+`;
+
+export const MovieReleaseDate = styled.p`
+  font-size: 0.9rem;
+  line-height: 1.7;
+  margin-bottom: -0.9rem;
+  opacity: 0.9;
+`;
+
+export const MovieRate = styled.p`
+  font-size: 0.9rem;
+  opacity: 0.9;
+`;
+
+export const MovieOverview = styled.p`
+  font-size: 1rem;
+  line-height: 1.7;
+
+  @media (max-height: 768px) {
+    font-size: 16px;
+    overflow-y: scroll !important;
+  }
+`;
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,67 @@
-const Modal = () => {
-  return <div></div>;
+import * as S from './Modal.style';
+import { Dispatch, SetStateAction, useEffect } from 'react';
+
+interface Movie {
+  id?: number;
+  title?: string;
+  name?: string;
+  original_name?: string;
+  overview?: string;
+  backdrop_path?: string;
+  release_date?: string;
+  first_air_date?: string;
+  vote_average?: string;
+}
+
+interface ModalProps {
+  setModalOpen: Dispatch<SetStateAction<boolean>>;
+  movie?: Movie;
+  genre: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ setModalOpen, movie, genre }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  return (
+    <S.Presentation role='presentation'>
+      <S.Container>
+        <S.Modal>
+          <S.Close onClick={() => setModalOpen(false)}>✕</S.Close>
+          <S.MovieBackdrop
+            src={
+              movie?.backdrop_path
+                ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+                : ''
+            }
+            alt={movie?.name || movie?.original_name || movie?.title}
+          />
+          <S.MovieContents>
+            <S.MovieGenre>
+              <p>| {genre}</p>
+            </S.MovieGenre>
+            <S.MovieTitle>
+              {movie?.name || movie?.title || movie?.original_name}
+            </S.MovieTitle>
+            <S.MovieReleaseDate>
+              개봉일:
+              {movie?.release_date
+                ? movie?.release_date
+                : movie?.first_air_date}
+            </S.MovieReleaseDate>
+            <S.MovieRate>
+              평점: {Math.floor(Number(movie?.vote_average) * 10) / 10}
+            </S.MovieRate>
+            <S.MovieOverview>{movie?.overview}</S.MovieOverview>
+          </S.MovieContents>
+        </S.Modal>
+      </S.Container>
+    </S.Presentation>
+  );
 };
 
 export default Modal;
