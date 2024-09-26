1b50bd230d2882e82b356bb78e8766afb3596f91
moeyg <moooeyg@gmail.com>
2024-09-26 17:13:07 +0900

    feat: 로그인 기능 추가

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,6 +1,20 @@
+import { auth } from '../../firebase';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import * as S from './Login.style';
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error('Error google login: ', error);
+      });
+  };
+
   return (
     <S.LoginPage>
       <S.VisualEffect autoPlay loop muted>
@@ -17,7 +31,7 @@ const Login = () => {
             Super selection, super easy
           </S.Description>
         </S.Header>
-        <S.LoginButton>Google Login</S.LoginButton>
+        <S.LoginButton onClick={handleAuth}>Google Login</S.LoginButton>
       </S.Container>
     </S.LoginPage>
   );
