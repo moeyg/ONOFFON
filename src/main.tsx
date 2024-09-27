df766c4bb8cddf2bd23ca004901233b20e1d0c72
moeyg <moooeyg@gmail.com>
2024-09-27 11:04:45 +0900

    fix: firebase app import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import app from './firebase.ts';
+import './firebase.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
